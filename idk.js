4484b0ffea9b277b42c35ce2474a622feeb80565
Your Name <you@example.com>
2025-09-19 11:29:02 +0300

    JS added selection

--- idk.js
+++ idk.js
@@ -1,17 +1,27 @@
 let setup = document.getElementById("setup");
 let delivery = document.getElementById("delivery");
 
+let selected = document.getElementById("select");
+
 let button = document.getElementById("generateBTN");
 
 button.addEventListener("click", async function () {
     console.log("clicked");
+    let data;
 
-    let data = await fetch("https://v2.jokeapi.dev/joke/Any?type=twopart");
-    let json = await data.json();
+    if(selected.value == "twoLiner"){
+        data = await fetch("https://v2.jokeapi.dev/joke/Any?type=twopart");
+        let json = await data.json();
+        setup.innerHTML = json.setup.toString();
+        delivery.innerHTML = json.delivery.toString()
+    } else{
+        data = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+        let json = await data.json();
+        setup.innerHTML = json.joke.toString();
+        delivery.innerHTML = null;
+    }
+    
     console.log(json);
-
-    setup.innerHTML = json.setup.toString();
-    delivery.innerHTML = json.delivery.toString();
 
     // try {
     //     const res = getData();
